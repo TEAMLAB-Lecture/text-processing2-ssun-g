16bdbda40644d65f2eb5761e69fb1f5cdf71f719
unknown <goflrkqk@naver.com>
2021-01-19 19:57:29 +0900

    Update my code

--- text_processing2.py
+++ text_processing2.py
@@ -83,10 +83,10 @@ def to_camel_case(underscore_str):
             "alreadyCamel"
     """
     camelcase_str = ""
-    temp_str = underscore_str.split('_')
+    temp_str = underscore_str.strip('_').split('_')
     for i in range(len(temp_str)):
         if i == 0:
-            camelcase_str += temp_str[i]
+            camelcase_str += temp_str[i].lower()
         else:
             camelcase_str += temp_str[i].capitalize()
 
